6a8b0a9c29f29053ee46d5f9064f403b81932e00
Karlee <karlee.lewis010@gmail.com>
2021-03-14 22:06:30 -0600

    pushing

---
+++ persona.js
@@ -0,0 +1,17 @@
+
+
+const toggleColorMode = e => {
+  const isChecked = e.target.checked;
+  if (isChecked) {
+    document.documentElement.setAttribute("color-mode", "light");
+    localStorage.setItem("color-mode", "light");
+    return;
+  }
+  document.documentElement.setAttribute("color-mode", "dark");
+  localStorage.setItem("color-mode", "dark");
+}
+
+const toggleColorButtons = document.querySelectorAll(".mode-toggle-btn");
+toggleColorButtons.forEach(btn => {
+  btn.addEventListener("input", toggleColorMode);
+});
